3ba16238fb278d6a9f983cf7c6f4bca1f6fd8b65
eshwarne <northeasteditsofficial@gmail.com>
2021-01-07 00:35:27 +0530

    setup graphql server using express-graphql

--- server/app.js
+++ server/app.js
@@ -1,4 +1,11 @@
 const express = require("express")
+const {graphqlHTTP} = require("express-graphql")
+
 const app = express()
+
+//graphql in express is used as middleware
+app.use("/graphql",graphqlHTTP({
+
+}))
 
 app.listen(5000)
